09c074f0f60ba6ddf32d204b6413f896ef64cc35
shantanu61000 <shantanutripathi96@gmail.com>
2020-06-01 02:18:54 +0530

    razorpay successfuly integrated

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_YQXyQjMfWUJZlZ';
+$keySecret = 'z9fJFoThk1c1Pw6KxXP7hgf8';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
---
+++ pay.php
@@ -0,0 +1,71 @@
+<?php
+
+require('config.php');
+require('razorpay-php/Razorpay.php');
+session_start();
+
+// Create the Razorpay Order
+
+use Razorpay\Api\Api;
+
+$api = new Api($keyId, $keySecret);
+
+//
+// We create an razorpay order using orders api
+// Docs: https://docs.razorpay.com/docs/orders
+//
+
+$orderData = [
+    'receipt'         => rand(1000,10000000),
+    'amount'          => $_POST['amt'] * 100, // 2000 rupees in paise
+    'currency'        => 'INR',
+    'payment_capture' => 1 // auto capture
+];
+
+$razorpayOrder = $api->order->create($orderData);
+
+$razorpayOrderId = $razorpayOrder['id'];
+
+$_SESSION['razorpay_order_id'] = $razorpayOrderId;
+
+$displayAmount = $amount = $orderData['amount'];
+
+if ($displayCurrency !== 'INR')
+{
+    $url = "https://api.fixer.io/latest?symbols=$displayCurrency&base=INR";
+    $exchange = json_decode(file_get_contents($url), true);
+
+    $displayAmount = $exchange['rates'][$displayCurrency] * $amount / 100;
+}
+
+
+$data = [
+    "key"               => $keyId,
+    "amount"            => $amount,
+    "name"              => $_POST['name'],
+    "description"       => $_POST['desc'],
+    "image"             => "https://s29.postimg.org/r6dj1g85z/daft_punk.jpg",
+    "prefill"           => [
+    "name"              => $_POST['name'],
+    "email"             => $_POST['email'],
+    "contact"           => $_POST['number'],
+    ],
+    "notes"             => [
+    "address"           => "blank addr",
+    "merchant_order_id" => "12312321",
+    ],
+    "theme"             => [
+    "color"             => "#f09300"
+    ],
+    "order_id"          => $razorpayOrderId,
+];
+
+if ($displayCurrency !== 'INR')
+{
+    $data['display_currency']  = $displayCurrency;
+    $data['display_amount']    = $displayAmount;
+}
+
+$json = json_encode($data);
+
+require("checkout/automatic.php");
